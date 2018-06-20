edd854170bc9b28849ba639ea2ecf1cc00bba2fe
Sulav Acharya <sulav.acharya@ampcus.com>
2018-06-20 10:18:41 -0400

    Updated Urls

--- firstDjangoProject/urls.py
+++ firstDjangoProject/urls.py
@@ -20,5 +20,4 @@ from django.conf.urls import url, include
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^post/', include('post.urls')),
-    url(r'^?$', include('post.urls')),
 ]
